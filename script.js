eb15ce5241686559cb9ba2e82bb40657c21ff487
AMER <amerbellil18@gmail.com>
2024-01-21 16:58:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/**
+ * Weather App
+ */
+
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ */
+
+
+const getWeatherData = async (city) => {
+    const URL = "https://api.openweathermap.org/data/2.5/weather";
+    const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+    const response = await fetch(FULL_URL);
+    const weatherData = await response.json();
+    return weatherData;
+  }
+
+/**
+ * Retrieve city input and get the weather data
+ */
+const searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("Something happend");
+  })
+}
+
+/**
+ * Show the weather data in HTML
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+}
+
